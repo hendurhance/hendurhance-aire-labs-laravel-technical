c63ee92d9e6ecac808b660c04497dec7221c8333
Endurance, the Martian <afriviral@gmail.com>
2023-08-26 20:42:21 +0100

    changes/ cleanups and component initialization (rating, testimonial)

---
+++ app/Livewire/TestimonialComponent.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class TestimonialComponent extends Component
+{
+    public function render()
+    {
+        return view('livewire.testimonial-component');
+    }
+}
