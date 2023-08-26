9bd0e12584f706c2b5153619c2a43acfcb528b0f
Endurance, the Martian <afriviral@gmail.com>
2023-08-26 20:43:57 +0100

    added/ testimonial semantic & data

--- app/Livewire/TestimonialComponent.php
+++ app/Livewire/TestimonialComponent.php
@@ -6,8 +6,62 @@ use Livewire\Component;
 
 class TestimonialComponent extends Component
 {
+    protected array $testimonials;
+
+    public function __construct()
+    {
+        $this->testimonials = [
+            [
+                'name' => 'Brittan Wheeler',
+                'designation' => 'Consultant',
+                'image' => asset('images/Brittan_Wheeler.png'),
+                'description' => 'You made it so simple.',
+                'rating' => 4,
+            ],
+            [
+                'name' => 'Alexa Popen',
+                'designation' => 'CEO',
+                'image' => asset('images/Alexa_Popen.png'),
+                'description' => 'You made it so simple. My new site is so much faster and easier to work with than my old site. I just choose the page, make the change & it is a good channel for us.',
+                'rating' => 3,
+            ],
+            [
+                'name' => 'Leslie Alexander',
+                'designation' => 'Founder',
+                'image' => asset('images/Leslie_Alexander.png'),
+                'description' => 'You made it so simple. My new site is so much faster and easier to work with than my old site. I just choose the page, make the change & it is a good channel for us.',
+                'rating' => 5,
+            ],
+            [
+                'name' => 'Leslie Alexander',
+                'designation' => 'Founder',
+                'image' => asset('images/Leslie_Alexander.png'),
+                'description' => 'You made it so simple. My new site is so much faster and easier to work with than my old site. I just choose the page, make the change & it is a good channel for us.',
+                'rating' => 5,
+            ],
+            [
+                'name' => 'Leslie Alexander',
+                'designation' => 'Founder',
+                'image' => asset('images/Leslie_Alexander.png'),
+                'description' => 'You made it so simple. My new site is so much faster and easier to work with than my old site. I just choose the page, make the change & it is a good channel for us.',
+                'rating' => 5,
+            ],
+            [
+                'name' => 'Leslie Alexander',
+                'designation' => 'Founder',
+                'image' => asset('images/Leslie_Alexander.png'),
+                'description' => 'You made it so simple. My new site is so much faster and easier to work with than my old site. I just choose the page, make the change & it is a good channel for us.',
+                'rating' => 5,
+            ],
+        ];
+    }
+
     public function render()
     {
-        return view('livewire.testimonial-component');
+        return view('livewire.testimonial-component', [
+            'testimonials' => collect($this->testimonials)->map(function ($testimonial) {
+                return (object) $testimonial;
+            }),
+        ]);
     }
 }
--- resources/views/livewire/testimonial-component.blade.php
+++ resources/views/livewire/testimonial-component.blade.php
@@ -1,3 +1,17 @@
-<div>
-    {{-- Nothing in the world is as soft and yielding as water. --}}
+<div class="p-10 py-8 mx-auto max-w-7xl">
+    <div class="grid gap-8">
+        @foreach ($testimonials as $testimonial)
+        <div class="item flex bg-white">
+            {{-- rating comes here --}}
+            <blockquote>"{{ $testimonial->description }}"</blockquote>
+            <div class="flex">
+                <img src="{{ $testimonial->image }}" alt="" class="avatar">
+                <div>
+                    <p class="font-semibold">{{ $testimonial->name }}</p>
+                    <p class="text-gray-600">{{ $testimonial->designation }}</p>
+                </div>
+            </div>
+        </div>
+        @endforeach
+    </div>
 </div>
